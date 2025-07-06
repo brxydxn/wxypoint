bdb34134126ebd0c282354146bc3d40b43e2f986
brxydxn <brayden@wxypoint.com>
2025-07-06 19:19:19 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return React.createElement("div", {
+    style: {
+      display: "flex",
+      height: "100vh",
+      justifyContent: "center",
+      alignItems: "center",
+      background: "black",
+      color: "white",
+      fontSize: "2rem"
+    }
+  }, "🚀 WXYPOINT Immersive Site Coming Soon — GitHub Hosting Test Successful");
+}
